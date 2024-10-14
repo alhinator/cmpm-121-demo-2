f22fe87e609b322a0a44637aa3092a246b3bfe86
alhinator <alex@leghart.org>
2024-10-14 16:53:27 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,14 @@ const APP_NAME = "Sticker Sketchpad aleghart";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+
+//create title
+const mainHeader = document.createElement("h1");
+mainHeader.innerText = APP_NAME;
+app.appendChild(mainHeader)
+
+const mainCanvas = document.createElement("canvas");
+mainCanvas.setAttribute("width", '256px');
+mainCanvas.setAttribute("height", '256px');
+app.appendChild(mainCanvas)
