9b8c936d847daa829edb9b29509246aa1134a5eb
alhinator <alex@leghart.org>
2024-10-15 16:08:56 -0700

    code cleanup

--- src/main.ts
+++ src/main.ts
@@ -16,37 +16,43 @@ mainCanvas.setAttribute("height", "256px");
 app.appendChild(mainCanvas);
 
 //cursor code modified from https://quant-paint.glitch.me/paint0.html
-const ctx = mainCanvas.getContext("2d");
-if (ctx == undefined) {
+const canvasContext = mainCanvas.getContext("2d");
+if (canvasContext == undefined) {
     throw "Canvas 2d context undefined.";
 }
 
+//create cursor objecct
 const cursor = { active: false, x: 0, y: 0 };
 
+//when mouse is pressed, start active drawing
 mainCanvas.addEventListener("mousedown", (e) => {
     cursor.active = true;
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
 });
 
+//if mouse is active and moving, draw at its position
 mainCanvas.addEventListener("mousemove", (e) => {
     if (cursor.active) {
-        ctx.beginPath();
-        ctx.moveTo(cursor.x, cursor.y);
-        ctx.lineTo(e.offsetX, e.offsetY);
-        ctx.stroke();
+        canvasContext.beginPath();
+        canvasContext.moveTo(cursor.x, cursor.y);
+        canvasContext.lineTo(e.offsetX, e.offsetY);
+        canvasContext.stroke();
         cursor.x = e.offsetX;
         cursor.y = e.offsetY;
     }
 });
 
+//stop drawing on mouseup
 mainCanvas.addEventListener("mouseup", (e) => {
     cursor.active = false;
 });
 
-const divider = document.createElement("div")
-const clearCanvasButton = document.createElement("button")
-clearCanvasButton.innerText = "CLEAR"
-clearCanvasButton.addEventListener("click", ()=>{ctx.clearRect(0,0,mainCanvas.width, mainCanvas.height)})
-divider.appendChild(clearCanvasButton)
-app.appendChild(divider)
+const divider = document.createElement("div");
+const clearCanvasButton = document.createElement("button");
+clearCanvasButton.innerText = "CLEAR";
+clearCanvasButton.addEventListener("click", () => {
+    canvasContext.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+});
+divider.appendChild(clearCanvasButton);
+app.appendChild(divider);
