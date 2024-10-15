e0685c190d4f6a4da604480a30c32be410a5bbab
alhinator <alex@leghart.org>
2024-10-15 16:06:36 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,48 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 
-
 //create title
 const mainHeader = document.createElement("h1");
 mainHeader.innerText = APP_NAME;
-app.appendChild(mainHeader)
+app.appendChild(mainHeader);
 
 const mainCanvas = document.createElement("canvas");
-mainCanvas.setAttribute("width", '256px');
-mainCanvas.setAttribute("height", '256px');
-app.appendChild(mainCanvas)
+mainCanvas.setAttribute("width", "256px");
+mainCanvas.setAttribute("height", "256px");
+app.appendChild(mainCanvas);
+
+//cursor code modified from https://quant-paint.glitch.me/paint0.html
+const ctx = mainCanvas.getContext("2d");
+if (ctx == undefined) {
+    throw "Canvas 2d context undefined.";
+}
+
+const cursor = { active: false, x: 0, y: 0 };
+
+mainCanvas.addEventListener("mousedown", (e) => {
+    cursor.active = true;
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+});
+
+mainCanvas.addEventListener("mousemove", (e) => {
+    if (cursor.active) {
+        ctx.beginPath();
+        ctx.moveTo(cursor.x, cursor.y);
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+        cursor.x = e.offsetX;
+        cursor.y = e.offsetY;
+    }
+});
+
+mainCanvas.addEventListener("mouseup", (e) => {
+    cursor.active = false;
+});
+
+const divider = document.createElement("div")
+const clearCanvasButton = document.createElement("button")
+clearCanvasButton.innerText = "CLEAR"
+clearCanvasButton.addEventListener("click", ()=>{ctx.clearRect(0,0,mainCanvas.width, mainCanvas.height)})
+divider.appendChild(clearCanvasButton)
+app.appendChild(divider)
