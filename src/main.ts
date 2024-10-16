5d5a426394e0ee4a67621087adfae8a936469d2a
alhinator <alex@leghart.org>
2024-10-16 14:19:33 -0700

    step 3 done

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,9 @@ mainCanvas.setAttribute("width", "256px");
 mainCanvas.setAttribute("height", "256px");
 app.appendChild(mainCanvas);
 
-//cursor code modified from https://quant-paint.glitch.me/paint0.html
+/*cursor code modified from https://quant-paint.glitch.me/paint0.html, 
+https://quant-paint.glitch.me/paint1.html
+*/
 const canvasContext = mainCanvas.getContext("2d");
 if (canvasContext == undefined) {
     throw "Canvas 2d context undefined.";
@@ -24,6 +26,15 @@ if (canvasContext == undefined) {
 //create cursor objecct
 const cursor = { active: false, x: 0, y: 0 };
 
+type Coord = {
+    x: number;
+    y: number;
+};
+const lines: Coord[][] = [];
+const redoLines: Coord[][] = [];
+
+let currLine: Coord[] = [];
+
 //create array of points to store mouse data in.
 
 //when mouse is pressed, start active drawing
@@ -31,30 +42,95 @@ mainCanvas.addEventListener("mousedown", (e) => {
     cursor.active = true;
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
+
+    //initialize
+    currLine = [{ x: cursor.x, y: cursor.y }];
+
+    //push when mouse goes down
+    lines.push(currLine);
+
+    //splice the redo lines
+    redoLines.splice(0, redoLines.length);
+    mainCanvas.dispatchEvent(drawChanged);
 });
 
 //if mouse is active and moving, draw at its position
 mainCanvas.addEventListener("mousemove", (e) => {
     if (cursor.active) {
-        canvasContext.beginPath();
-        canvasContext.moveTo(cursor.x, cursor.y);
-        canvasContext.lineTo(e.offsetX, e.offsetY);
-        canvasContext.stroke();
         cursor.x = e.offsetX;
         cursor.y = e.offsetY;
+
+        currLine.push({ x: cursor.x, y: cursor.y });
+
+        mainCanvas.dispatchEvent(drawChanged);
     }
 });
 
 //stop drawing on mouseup
 mainCanvas.addEventListener("mouseup", (e) => {
     cursor.active = false;
+    currLine = [];
+    mainCanvas.dispatchEvent(drawChanged);
 });
 
-const divider = document.createElement("div");
+//canvas dispatch event
+const drawChanged = new Event("drawing-changed");
+
+mainCanvas.addEventListener("drawing-changed", (e) => {
+    canvasContext.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+
+    for (const line of lines) {
+        if (line.length > 1) {
+            canvasContext.beginPath();
+            const { x, y } = line[0];
+            canvasContext.moveTo(x, y);
+            for (const { x, y } of line) {
+                canvasContext.lineTo(x, y);
+            }
+            canvasContext.stroke();
+        }
+    }
+});
+
+const buttonSection = document.createElement("div");
+//do clear, undo, redo buttons
 const clearCanvasButton = document.createElement("button");
+buttonSection.appendChild(clearCanvasButton);
 clearCanvasButton.innerText = "CLEAR";
 clearCanvasButton.addEventListener("click", () => {
     canvasContext.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+    lines.splice(0, lines.length);
+    redoLines.splice(0, redoLines.length);
+
+    mainCanvas.dispatchEvent(drawChanged);
 });
-divider.appendChild(clearCanvasButton);
-app.appendChild(divider);
+
+const undoButton = document.createElement("button");
+buttonSection.appendChild(undoButton);
+undoButton.innerText = "UNDO";
+undoButton.addEventListener("click", () => {
+    canvasContext.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+    let tmp = lines.pop();
+    if (typeof tmp == "object") {
+        redoLines.push(tmp);
+        console.log(typeof tmp, "pushed")
+    }
+    mainCanvas.dispatchEvent(drawChanged);
+});
+
+const redoButton = document.createElement("button");
+buttonSection.appendChild(redoButton);
+redoButton.innerText = "REDO";
+redoButton.addEventListener("click", () => {
+    canvasContext.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+    if (redoLines.length > 0) {
+        let tmp = redoLines.pop();
+        if (typeof tmp == "object") {
+            lines.push(tmp);
+        }
+    }
+
+    mainCanvas.dispatchEvent(drawChanged);
+});
+
+app.appendChild(buttonSection);
